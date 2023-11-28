7868a2bc2ec9d073b18409206fd9d5412c616063
tanul <tanulgupta3@gmail.com>
2023-11-28 12:33:54 +0800

    Completed permissions

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const Datastore = require('nedb');
-const app = express();
 const crypto = require('crypto');
+const app = express();
 
 app.use(express.static('public')); // Serve static files from 'public' directory
 app.use(express.json()); // Middleware to parse JSON bodies
 
-// Initialize NeDB database
+// Initialize NeDB databases
 const db = new Datastore({ filename: 'events.db', autoload: true });
 const permissionsDb = new Datastore({ filename: 'permissions.db', autoload: true });
 
-// API endpoint to store events
-app.post('/api/events', (req, res) => {
+// Middleware to check for a valid session
+const checkSession = (req, res, next) => {
+    const sessionId = req.headers['session-id'];
+    if (!sessionId) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    permissionsDb.findOne({ sessionId }, (err, user) => {
+        if (err || !user) {
+            return res.status(401).json({ message: 'Invalid session' });
+        }
+        next();
+    });
+};
+
+// API endpoint to store events (protected)
+app.post('/api/events', checkSession, (req, res) => {
     const event = req.body;
     db.insert(event, (err, newDoc) => {
         if (err) {
@@ -33,8 +48,8 @@ app.get('/api/events', (req, res) => {
     });
 });
 
-// API endpoint to delete an event
-app.delete('/api/events/:id', (req, res) => {
+// API endpoint to delete an event (protected)
+app.delete('/api/events/:id', checkSession, (req, res) => {
     db.remove({ _id: req.params.id }, {}, (err, numRemoved) => {
         if (err) {
             res.status(500).send(err);
@@ -44,9 +59,9 @@ app.delete('/api/events/:id', (req, res) => {
     });
 });
 
-// API endpoint to update an event
-app.put('/api/events/:id', (req, res) => {
-    db.update({ _id: req.params.id }, { $set: { eventName: req.body.eventName, eventHub: req.body.eventHub }}, {}, (err, numReplaced) => {
+// API endpoint to update an event (protected)
+app.put('/api/events/:id', checkSession, (req, res) => {
+    db.update({ _id: req.params.id }, { $set: req.body }, {}, (err, numReplaced) => {
         if (err) {
             res.status(500).send({ error: err.message });
         } else {
@@ -55,16 +70,15 @@ app.put('/api/events/:id', (req, res) => {
     });
 });
 
+// API endpoint for user login
 app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
     permissionsDb.findOne({ username, password }, (err, doc) => {
         if (err) {
             res.status(500).send(err);
         } else if (doc) {
-            // Generate a random session ID
             const sessionId = crypto.randomBytes(16).toString('hex');
-            // Store session information
-            permissionsDb.update({ username }, { $set: { sessionId } }, {}, (err, numUpdated) => {
+            permissionsDb.update({ username }, { $set: { sessionId } }, {}, (err) => {
                 if (err) {
                     res.status(500).send(err);
                 } else {
@@ -77,48 +91,17 @@ app.post('/api/login', (req, res) => {
     });
 });
 
-// const commonPassword = "MstrPwd123"; // Common password for all users
-// const hubs = ["DHKL", "DHM", "DHN", "DHMV", "DHT", "HQ"]; // List of HUBs
+app.post('/api/logout', (req, res) => {
+    const sessionId = req.headers['session-id'];
+    permissionsDb.update({ sessionId }, { $unset: { sessionId: 1 } }, {}, (err) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).json({ message: 'Logged out successfully' });
+        }
+    });
+});
 
-// // Generate user objects with usernames and common password
-// const users = hubs.map(hub => {
-//     return {
-//         username: `${hub}@em1`,
-//         password: commonPassword // In real applications, passwords should be hashed
-//     };
-// });
-
-// // Function to insert users
-// function initializeUsers() {
-//     // Remove all existing users (optional, based on your requirement)
-//     permissionsDb.remove({}, { multi: true }, function (err, numRemoved) {
-//         // Insert new users
-//         permissionsDb.insert(users, function (err, newDocs) {
-//             if (err) {
-//                 console.error("Error inserting users:", err);
-//             } else {
-//                 console.log("Added users:", newDocs.length);
-//             }
-//         });
-//     });
-// }
-
-// // Call the function to initialize users
-// initializeUsers();
-
-// app.post('/api/login', (req, res) => {
-//     const { username, password } = req.body;
-
-//     permissionsDb.findOne({ username: username }, (err, user) => {
-//         if (err) {
-//             res.status(500).send("Error accessing the database");
-//         } else if (user && user.password === password) {
-//             res.status(200).json({ message: 'Login successful' });
-//         } else {
-//             res.status(401).json({ message: 'Invalid credentials' });
-//         }
-//     });
-// });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- public/script.js
+++ public/script.js
@@ -20,6 +20,12 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         console.error('Login button not found');
     }
+
+     // Event listener for the logout button
+     const logoutButton = document.getElementById('logoutButton');
+     logoutButton.addEventListener('click', function() {
+         logout();
+     });
 });
 
 function generateCalendar(calendarEl, year) {
@@ -81,9 +87,15 @@ function generateMonth(year, month) {
 
 
 function openModal(year, month, day) {
-    selectedDate = { year, month, day };
-    document.getElementById('eventModal').style.display = 'block';
+    // Check if the logout button is visible, indicating that the user is logged in
+    if (document.getElementById('logoutButton').style.display === 'block') {
+        selectedDate = { year, month, day };
+        document.getElementById('eventModal').style.display = 'block';
+    } else {
+        alert("Please log in to add or edit events.");
+    }
 }
+
 
 window.saveEvent = function() {
     const eventName = document.getElementById('eventName').value;
@@ -98,6 +110,7 @@ window.saveEvent = function() {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Session-ID': localStorage.getItem('sessionId')  // Include session ID
         },
         body: JSON.stringify(eventData),
     })
@@ -109,7 +122,7 @@ window.saveEvent = function() {
     .catch((error) => {
         console.error('Error:', error);
     });
-}
+};
 
 function loadEvents() {
     clearEvents(); // Clear existing events before loading new ones
@@ -155,8 +168,8 @@ function displayEvent(event) {
     }
 
     eventElement.addEventListener('click', function() {
-        openEditModal(event);
-    });
+            openEditModal(event);
+        });
 }
 
 function setEventColor(element, color, isPttEvent) {
@@ -169,33 +182,31 @@ function setEventColor(element, color, isPttEvent) {
 }
 
 function openEditModal(event) {
-    // Populate the modal with event data
-    document.getElementById('editEventName').value = event.eventName;
-    document.getElementById('editEventHub').value = event.eventHub;
-    
-    // Show the modal
-    document.getElementById('editEventModal').style.display = 'block';
-
-    // Attach event ID to the modal for reference
-    document.getElementById('editEventModal').setAttribute('data-event-id', event._id);
+    // Check if the user is logged in by verifying the visibility of the logout button
+    if (document.getElementById('logoutButton').style.display === 'block') {
+        // Populate the modal with event data
+        document.getElementById('editEventName').value = event.eventName;
+        document.getElementById('editEventHub').value = event.eventHub;
+        // Show the modal
+        document.getElementById('editEventModal').style.display = 'block';
+        // Attach event ID to the modal for reference
+        document.getElementById('editEventModal').setAttribute('data-event-id', event._id);
+    } else {
+        alert("Please log in to edit events.");
+    }
 }
-// Additional code for handling the modal, etc.
+
 
 window.saveEditedEvent = function() {
     const eventId = document.getElementById('editEventModal').getAttribute('data-event-id');
     const eventName = document.getElementById('editEventName').value.trim();
     const eventHub = document.getElementById('editEventHub').value;
 
-    // Check if the event name is empty
-    if (!eventName) {
-        alert("Event name cannot be empty.");
-        return;
-    }
-
     fetch(`/api/events/${eventId}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
+            'Session-ID': localStorage.getItem('sessionId')  // Include session ID
         },
         body: JSON.stringify({ eventName, eventHub }),
     })
@@ -215,10 +226,16 @@ window.saveEditedEvent = function() {
 
 
 
+
 function deleteEvent() {
     const eventId = document.getElementById('editEventModal').getAttribute('data-event-id');
     if (confirm('Are you sure you want to delete this event?')) {
-        fetch(`/api/events/${eventId}`, { method: 'DELETE' })
+        fetch(`/api/events/${eventId}`, {
+            method: 'DELETE',
+            headers: {
+                'Session-ID': localStorage.getItem('sessionId')  // Include session ID
+            }
+        })
         .then(response => {
             if(response.ok) {
                 document.getElementById('editEventModal').style.display = 'none';
@@ -230,6 +247,7 @@ function deleteEvent() {
         .catch(error => console.error('Error:', error));
     }
 }
+
 
 
 // Login functionality
@@ -245,16 +263,55 @@ function login() {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            
         },
         body: JSON.stringify({ username, password }),
     })
     .then(response => response.json())
     .then(data => {
-        // Handle login success or failure here
-        console.log(data);
+        if (data.sessionId) {
+            localStorage.setItem('sessionId', data.sessionId);
+           // Show logout button and hide login button
+        document.getElementById('logoutButton').style.display = 'block';
+        document.getElementById('loginButton').style.display = 'none';
+        }
         closeLoginModal();
     })
     .catch((error) => {
         console.error('Error:', error);
     });
+}
+
+
+function getAuthHeaders() {
+    const sessionId = localStorage.getItem('sessionId');
+    return sessionId ? { 'Session-ID': sessionId } : {};
+}
+
+function logout() {
+    const sessionId = localStorage.getItem('sessionId');
+    if (sessionId) {
+        fetch('/api/logout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Session-ID': sessionId  // Include session ID
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                console.log('Logged out successfully');
+            } else {
+                console.error('Logout failed');
+            }
+        })
+        .catch(error => console.error('Error:', error));
+
+        // Remove the session ID from local storage
+        localStorage.removeItem('sessionId');
+
+        // Hide the logout button and show the login button
+        document.getElementById('logoutButton').style.display = 'none';
+        document.getElementById('loginButton').style.display = 'block';
+    }
 }
